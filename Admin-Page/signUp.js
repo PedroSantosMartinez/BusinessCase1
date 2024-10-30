bedf6d77fe1ee775d7570f9d27c486035e023668
Pedro.S <psant0109@launchpadphilly.org>
2024-10-30 14:57:01 -0400

    Cleaner Organize of Files

--- Admin-Page/signUp.js
+++ Admin-Page/signUp.js
@@ -2,10 +2,10 @@ document.getElementById('userForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form from submitting
 
     // Get the values entered in the form
-    const name = document.getElementById('name').value;
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
+    const name = document.getElementById('name').value.trim();
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('phone').value;
     const authority = document.getElementById('authority').value;
 
     // You can now process these values, like sending them to a server or displaying a message
